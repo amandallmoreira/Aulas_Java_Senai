8346f8df0fa2d6b7371f221e4c0e401c361d066a
amandallmoreira <amandallmoreira@gmail.com>
2026-07-09 21:34:50 -0300

    Atualizando arquivos no repositório

--- src/SegundaAula.java
+++ src/SegundaAula.java
@@ -2,12 +2,30 @@ import java.util.Scanner;
 
 public class SegundaAula {
     public static void main(String[] args){
-        Scanner entrada = new Scanner (System.in);
+        //Scanner = input
+        Scanner scanner = new Scanner(System.in);
+        int valor = scanner.nextInt();
 
-        int idade=entrada.nextInt();
-        entrada.nextLine();
+        //Usabilidade do if-> pode ter duas respostas
+        System.out.println("\n \n Utilizando a comparação do If Else");
+        if(valor==1){
+            System.out.println("Menor que 10");
+        }else {
+            System.out.println("Maior que 10");
+        }
 
-        double altura = entrada.nextDouble();
-        entrada.nextLine();
-    }
+        //Switch case -> pode fazer várias comparações
+        System.out.println("\n \n Utilizando a comparação do Switch Case");
+        switch (valor){
+            case 1:
+                System.out.println("Escolheu 1");
+                break;
+            case 2:
+                System.out.println("Escolheu 2");
+                break;
+            default:
+                System.out.println("Não escolheu uma das opções!");
+                break;
+
+        }
 }
