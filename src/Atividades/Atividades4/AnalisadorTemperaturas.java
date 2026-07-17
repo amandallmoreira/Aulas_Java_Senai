d1ad212b201aea244467fa5b78e859503ebda867
amandallmoreira <amandallmoreira@gmail.com>
2026-07-16 21:46:03 -0300

    Atualização Atividades 3 e criação da Atividade 4

---
+++ src/Atividades/Atividades4/AnalisadorTemperaturas.java
@@ -0,0 +1,56 @@
+package Atividades.Atividades4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+2. Analisador de Temperaturas
+Objetivo: Criar um programa que utilize um vetor para armazenar registros de temperaturas e identifique
+qual foi a maior e a menor temperatura informada.
+
+Regras de Negócio:
+Declare um vetor do tipo decimal chamado temperaturas de tamanho 6 (new double[6]). OK
+Peça ao utilizador para digitar as 6 temperaturas consecutivas através do console (terminal).
+Percorra o vetor utilizando uma estrutura de repetição para encontrar o maior valor e o menor valor
+armazenados.
+Dica: compare os valores de cada posição do vetor com outras variáveis
+double maior = temperaturasVetor[0];
+double menor = temperaturasVetor[0];
+Imprima no console lista de temperaturas informadas, seguidas da maior e da menor temperatura
+encontradas.
+*/
+
+public class AnalisadorTemperaturas {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double[] temperatura = new double[6]; //array de double
+
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Digite a " + (i+1) + "ª temperatura: ");
+            temperatura[i] = scanner.nextDouble();
+        }
+
+        double maior = temperatura[0]; //primeiro item da lista
+        double menor = temperatura[0];
+
+        for (int i = 1; i < 6; i++) { //Não pode começar em 0, pois não pode começar comparando a temperatura consigo mesma
+            // i: Só guarda a posição do item, não o valor dele
+            if (maior <= temperatura[i]) { // temperatura[i]: o que está dentro do vetor, naquela posição
+                maior = temperatura[i]; //Salva o valor do vetor
+            }
+            if (menor >= temperatura[i]) {
+                menor = temperatura[i];
+            }
+
+        }
+
+        System.out.println("Temperaturas: " + Arrays.toString(temperatura));
+        System.out.println("Maior temperatura: " + maior + "°C");
+        System.out.println("Menor temperatura: " + menor + "°C");
+
+
+    }
+}
--- src/Atividades/Atividades4/SomaElementosVetor.java
+++ src/Atividades/Atividades4/SomaElementosVetor.java
@@ -1,8 +1,13 @@
 package Atividades.Atividades4;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.Scanner;
+/*Objetivo: Criar um programa que declare um vetor de inteiros com tamanho fixo, permita ao utilizador preencher todos os campos e exiba a soma total dos valores.
+Regras de Negócio:
+Declare um vetor de inteiros chamado numeros com tamanho 5 (new int[5]).
+Utilize um laço for para ler 5 números inteiros via Scanner e guardá-los nas posições do vetor.
+Após o preenchimento, utilize outro laço para somar todos os elementos contidos no vetor.
+Exiba o vetor formatado e o resultado da soma no console.
+*/
 
 public class SomaElementosVetor {
     public static void main(String[] args) {
